3a64d345e1a54da43e6aa7523d540c0cfe51f5f5
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-04-28 08:18:44 -0500

    Eliminación de hospital-system, integración de carpeta PruebaJuanLopez

--- hospital-system/src/app/admin/models/hospital.model.ts
+++
@@ -1,6 +0,0 @@
-export interface Hospital {
-  id: string;
-  nombre: string;
-  direccion: string;
-  urlApi: string;
-}
--- hospital-system/src/app/hospital/hospital.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HospitalRoutingModule } from './hospital-routing.module';
-import { ConsultasComponent } from './pages/consultas/consultas.component';
-
-@NgModule({
-  imports: [CommonModule, HospitalRoutingModule, ConsultasComponent],
-})
-export class HospitalModule {}
