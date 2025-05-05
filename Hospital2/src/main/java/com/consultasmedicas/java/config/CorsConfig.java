28baa5d97f68d00982e766f6ab4f0530a6228b10
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-05-05 11:48:46 -0500

    Se cambiaron las variables para que funcionen con azure

--- Hospital2/src/main/java/com/consultasmedicas/java/config/CorsConfig.java
+++ Hospital2/src/main/java/com/consultasmedicas/java/config/CorsConfig.java
@@ -11,6 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:62556")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
