08c6c9c8d33fb052c32d2bb516967965150709e7
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-05-05 11:44:52 -0500

    Se cambiaron las variables para que funcionen con azure

--- Hospital2/init.sql
+++ Hospital2/init.sql
@@ -1,8 +1,8 @@
 -- Crear el usuario solo si no existe
-CREATE USER IF NOT EXISTS 'hospital2_user'@'%' IDENTIFIED BY 'password123';
+CREATE USER IF NOT EXISTS 'hospital1_user'@'%' IDENTIFIED BY 'password123';
 
 -- Dar todos los privilegios sobre la base de datos `hospital1`
-GRANT ALL PRIVILEGES ON hospital2.* TO 'hospital2_user'@'%';
+GRANT ALL PRIVILEGES ON hospital1.* TO 'hospital1_user'@'%';
 
 -- Aplicar los cambios
 FLUSH PRIVILEGES;
--- Hospital2/src/main/java/com/consultasmedicas/java/security/SecurityConfig.java
+++ Hospital2/src/main/java/com/consultasmedicas/java/security/SecurityConfig.java
@@ -14,11 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -73,17 +68,5 @@ public class SecurityConfig {
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
-    }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200")); // o "*" si estás probando
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }
