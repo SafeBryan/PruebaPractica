ddd02c01a5d6cfdb3278d66798fd7e31a4f6b0d0
Bryan Pazmino <bryanpazmi1977@gmail.com>
2025-05-05 11:04:42 -0500

    Se cambiaron las variables para que funcionen con azure

--- Hospital2/src/main/java/com/consultasmedicas/java/config/SwaggerConfig.java
+++ Hospital2/src/main/java/com/consultasmedicas/java/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -28,6 +31,9 @@ public class SwaggerConfig {
                                         .name(securitySchemeName)
                                         .type(SecurityScheme.Type.HTTP)
                                         .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                                        .bearerFormat("JWT")))
+                .servers(List.of(
+                        new Server().url("https://hospital-cuenca.duckdns.org")
+                ));
     }
 }
